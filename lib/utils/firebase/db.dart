d26f214d004dd9110e4ff4dbdbfa351ae57a663b
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-12-26 23:41:14 +0800

    adding user and event firebase api

--- lib/utils/firebase/db.dart
+++ lib/utils/firebase/db.dart
@@ -11,4 +11,87 @@ class FirestoreService {
       "ical": icalLink
     });
   }
+
+  // ======================== USER API ======================== 
+  Future<String> getICalLink() async {
+    try {
+      DocumentSnapshot doc = await usersCollection.doc(uid).get();
+      var data = doc.data() as Map<String, dynamic>;
+      return data['ical'];
+    } catch (e) {
+      print(e);
+      throw Error();
+    }
+  }
+
+  // ======================== EVENTS API ======================== 
+  Future syncEvents(List<Map<String, dynamic>> IcalData) async {
+    try {
+      // clean data
+      IcalData.removeWhere((event) => 
+        event.containsKey("uid") &&
+        event.containsKey("summary"));
+      
+      // initialize firestore references
+      final batch = FirebaseFirestore.instance.batch();
+
+      // batch write
+      for (var doc in IcalData) {
+        DocumentReference eventsCollection = usersCollection.doc(uid).collection("events").doc(doc["uid"]);
+        batch.set(eventsCollection, {
+          "uid": doc["uid"],
+          "dtstart": doc["dtstart"],
+          "dtend": doc["dtend"],
+          "summary": doc["summary"],
+        });
+      }
+
+      await batch.commit();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future getAllEvents() async {
+    try {
+      QuerySnapshot doc = await usersCollection.doc(uid).collection("events").get();
+      final data = doc.docs.map((e) => e.data()) as Map<String, dynamic>;
+
+      return data;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future getEventsWhere() async {
+    try {
+
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future addEvents() async {
+    try {
+
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future updateEvents() async {
+    try {
+      
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future deleteEvents() async {
+    try {
+      
+    } catch (e) {
+      print(e);
+    }
+  }
 }
