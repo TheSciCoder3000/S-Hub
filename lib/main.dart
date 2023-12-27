0b48fe06645441bf72a67247ffc362727a99a763
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-12-27 22:16:57 +0800

    reorganized main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,18 +32,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool initializing = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 10), () {
-      setState(() {
-        initializing = false;
-      });
-    });
-  }
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -61,36 +49,71 @@ class _MyAppState extends State<MyApp> {
             create: (context) => EventState()
           )
         ],
-        child: StreamBuilder<SUser?>(
-          stream: AuthService().user,
-          builder: (context, snapshot) {
-            String? userUid = context.watch<SUser>().uid;
-            EventState eventState = context.watch<EventState>();
+        child: const StreamRouter()
+      )
+    );
+  }
+}
 
-            if (!initializing) {
-              if (snapshot.hasData) {
-                if (userUid != null) {
-                  if (!eventState.initializing) {
-                    return const MainWrapper();
-                  } else {
-                    FirestoreService(uid: userUid).getAllEvents()
-                    .then((eventList) {
-                      eventState.parse(eventList);
-                    });
-                    return const Splash();
-                  }
-                } else {
-                  return const AuthPage();
-                }
+class StreamRouter extends StatefulWidget {
+  const StreamRouter({super.key});
+
+  @override
+  State<StreamRouter> createState() => StreamRouterState();
+}
+
+class StreamRouterState extends State<StreamRouter> {
+  bool initializing = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 10), () {
+      setState(() {
+        initializing = false;
+      });
+    });
+  }
+
+  Future<void> initializeEvents(String userUid) async {
+    EventState eventState = context.read<EventState>();
+
+    await FirestoreService(uid: userUid).getAllEvents()
+    .then((eventList) {
+      eventState.parse(eventList);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<SUser?>(
+      stream: AuthService().user,
+      builder: (context, snapshot) {
+        String? userUid = context.watch<SUser>().uid;
+        EventState eventState = context.watch<EventState>();
+
+        if (!initializing) {
+          if (snapshot.hasData) {
+            if (userUid != null) {
+              if (!eventState.initializing) {
+                return const MainWrapper();
               } else {
+                if (eventState.eventMap.isEmpty) {
+                  print("event map is empty, running async functions");
+                  initializeEvents(userUid);
+                }
                 return const Splash();
               }
             } else {
-              return const Splash();
+              return const AuthPage();
             }
-          },
-        )
-      )
+          } else {
+            return const Splash();
+          }
+        } else {
+          return const Splash();
+        }
+      },
     );
   }
 }
